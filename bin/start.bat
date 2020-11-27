744974a4eaf050d7b3bd5b1d1484e2c3aa3c0a50
jofenxue <jofen0701324011>
2020-11-27 17:38:16 +0800

    添加dbsqls.xml文件的windows平台区分

--- bin/start.bat
+++ bin/start.bat
@@ -1,4 +1,4 @@
 @echo on
 
-C:\ProgramData\Oracle\Java\javapath\java.exe -Dfile.encoding=UTF-8 -classpath .:.\lib\* -XX:PermSize=256m -XX:MaxPermSize=512m -Xms1024m -Xmx1024m com.combat.core.Engine
+C:\ProgramData\Oracle\Java\javapath\java.exe -Dfile.encoding=UTF-8 -classpath .;.\lib\* -XX:PermSize=256m -XX:MaxPermSize=512m -Xms1024m -Xmx1024m com.combat.core.Engine
 pause
--- src/main/java/com/combat/core/Engine.java
+++ src/main/java/com/combat/core/Engine.java
@@ -1,6 +1,7 @@
 package com.combat.core;
 
 import com.combat.beans.Dbsqls;
+import com.combat.beans.Sql;
 import com.combat.handle.config.ConfigHandler;
 import com.combat.handle.config.PropertyConfigHandler;
 import com.combat.handle.db.DBHandler;
@@ -17,7 +18,9 @@ public class Engine {
         //获取sqlxml文件
         log.info("loading dbsqls.xml ......");
         XMLJaxbHandler h = XMLJaxbHandler.getHandler();
-        Dbsqls sqls = (Dbsqls)h.getXMLBeans("dbsqls.xml", Dbsqls.class);
+        String dbssql = isWindows()? "src/main/resources/dbsqls.xml" : "dbsqls.xml";
+        log.info("loading dbsqls.xml path: " + dbssql);
+        Dbsqls sqls = (Dbsqls)h.getXMLBeans(dbssql, Dbsqls.class);
 
         //获取db参数
         String dbcffile = sqls.getDbconfig().getConfigpath();
@@ -30,13 +33,20 @@ public class Engine {
         ConfigHandler confiH = PropertyConfigHandler.getHandler(dbcffile);
         confiH.getPropValue(url);
         log.info(confiH.getPropValue(url));
-        /*for(Sql sql : sqls.getSql()) {
+        for(Sql sql : sqls.getSql()) {
             log.debug(sql.getContent());
-        }*/
+        }
 
+        /*if(true) {
+            return;
+        }*/
 
         log.info("executing patch ......");
         DBHandler dh = DBHandler.getHandler(confiH.getPropValue(url), confiH.getPropValue(username), confiH.getPropValue(password));
         dh.executeUpdate(sqls.getSql());
+    }
+
+    public static boolean isWindows() {
+        return System.getProperties().getProperty("os.name").toUpperCase().indexOf("WINDOWS") != -1;
     }
 }
